c2d48e6c33df7b81b3e7e1d475827c370e973016
lukewire129 <lukewire129@gmail.com>
2024-01-26 11:27:18 +0900

    Platfrom Service X Navi Sample

---
+++ Sample/NavigatorBar/DemoApp4/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace DemoApp4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent ();
+            
+            List<string> a = new List<string> ();
+            for(int i=0; i<100; i++)
+            {
+                a.Add(i.ToString ());
+            }
+            items.ItemsSource = a;
+        }
+
+        private void items_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            if(e.VerticalDelta > 0)
+            {
+                navi.Opacity = 0.5;
+            }
+            else
+            {
+                navi.Opacity = 1;
+            }
+        }
+    }
+
+}
