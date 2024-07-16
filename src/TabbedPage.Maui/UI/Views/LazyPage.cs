929c477608b5b73074ef436d58bec2c1127f81a1
lukewire129 <lukewire129@gmail.com>
2024-07-16 17:00:32 +0900

    test ...

---
+++ src/TabbedPage.Maui/UI/Views/LazyPage.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Maui.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabbedPage.Maui.UI.Views
+{
+    public class CustomLazyView<TView> : LazyView where TView : View, new()
+    {
+        public override async ValueTask LoadViewAsync(CancellationToken token)
+        {
+            // display a loading indicator
+            Content = new ActivityIndicator { IsRunning = true }.Center ();
+
+            // simulate a long running task
+            await Task.Delay (3000, token);
+
+            // load the view
+            Content = new TView { BindingContext = BindingContext };
+
+            SetHasLazyViewLoaded (true);
+        }
+    }
+}
